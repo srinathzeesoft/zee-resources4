fc862db5793ce0f4fb82f49d6fe8a8f5ad8ac5be
srinathzeesoft <srinathzeesoft@gmail.com>
2026-07-20 17:23:40 +0530

    Update mkt2.0_messages.js

--- mkt2.0_messages.js
+++ mkt2.0_messages.js
@@ -1,5 +1,5 @@
 Mkt3L10n.AccountAnalyzerChart = {
-  Trend: "Tendência test test check",
+  Trend: "Tendência test test check check",
   Opportunity: "Oportunidade",
   Interactions: "Interações",
   Week_Of_Date_Interaction: "<b>Semana de {0} - ({1} interação{2})</b> ",
