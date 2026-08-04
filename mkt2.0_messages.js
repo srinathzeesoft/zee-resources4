560bf605d5637738155e6afe36c1e1d3943f58b3
srinathzeesoft <srinathzeesoft@gmail.com>
2026-08-04 12:57:11 +0530

    Update mkt2.0_messages.js

--- mkt2.0_messages.js
+++ mkt2.0_messages.js
@@ -1,5 +1,5 @@
 Mkt3L10n.AccountAnalyzerChart = {
-  Trend: "Tendência checkinggg checkingggg testing moooo gdsgsdgsd gdgdsfgsdfb gsdgsdgdsf gfdgfdgfdgb gdsfgdfgfdgfd"
+  Trend: "Tendência update hhhh jjjjjjjj ooooooooo llllllllll kkkkkkkkkkk  kkkkkkk"
   Opportunity: "Oportunidade test test test check check",
   Interactions: "Interações",
   Week_Of_Date_Interaction: "<b>Semana de {0} - ({1} interação{2})</b> ",
