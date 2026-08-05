657a2877f69ea7025eab0bc0ffeb775ac6539d8f
srinathzeesoft <srinathzeesoft@gmail.com>
2026-08-05 10:51:52 +0530

    Update mkt2.0_messages.js

--- mkt2.0_messages.js
+++ mkt2.0_messages.js
@@ -5,7 +5,7 @@ Mkt3L10n.AccountAnalyzerChart = {
   Week_Of_Date_Interaction: "<b>Semana de {0} - ({1} interação{2})</b> ",
   Date_Interaction: "<b>{0} - ({1} interação{2})</b> ",
   Interactions_Cumulative_: "Interações (cumulativo)",
-  Date_Interaction: "<b>{0} - ({1} interação{2})</b> ",
+  Date_Interaction: "<b>{0} - ({1} interação{2})</b> gggg ggg ggg ggg ggg gg gg gg ",
 };
 Mkt3L10n.analytics = {
   N_A: "N/D",
