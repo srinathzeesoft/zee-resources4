856d0f78c8ab786f07f416590766ddda1cdf2413
srinathzeesoft <srinathzeesoft@gmail.com>
2026-08-05 18:13:00 +0530

    Update mkt3.0_messages.js

--- mkt3.0_messages.js
+++ mkt3.0_messages.js
@@ -1,5 +1,5 @@
 Mkt3L10n.adminFieldHtmlEncodeForm = {
-  Html_Encode_Tokens: "HTML Encode Tokens in Emails",
+  Html_Encode_Tokens: "HTML Encode Tokens in Emails hhh hhh jjj kkk",
   Explain: "Configure the default HTML encoding behavior in email tokens for all fields",
   Remind: "This setting will apply to all fields. You can configure this setting for each field as well",
   Save: "SAVE",
